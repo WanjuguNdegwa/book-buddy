5d97c12573593290c237043ed24db76bc6d70697
WanjuguNdegwa <ann.ndegwa@yahoo.com>
2022-08-04 22:05:30 +0300

    Book model validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
+
+  validates :title, length: { minimum: 2 }, presence: true
+  validates :author, presence: true
+  validates :description, length: { minimum: 10 }
 end
