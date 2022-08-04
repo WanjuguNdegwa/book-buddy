84cf689b6d8daa54570d46c890a47c0b00d7131c
WanjuguNdegwa <ann.ndegwa@yahoo.com>
2022-08-04 22:05:15 +0300

    Add book delete method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   def index
     render json: Book.all
   end
 
   def show
-    book = Book.find_by(id: params[:id])
+    book = Book.find(params[:id])
 
     if book
       render json: book
-    else
-      render json: { error: "Book not found" }, status: :not_found
     end
   end
 
@@ -22,11 +23,30 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
 
     book.update!(book_params)
+
+    render json: book
+  end
+
+  def destroy
+    book = Book.find_by(id: params[:id])
+
+    if book
+      book.destroy
+      head :no_content
+    end
   end
 
   private
 
   def book_params
     params.permit(:title, :author, :genre, :image_url, :description)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    render json: { error: "Bird not found" }, status: :not_found
   end
 end
